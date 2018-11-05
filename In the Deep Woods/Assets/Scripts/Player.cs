cb0db0ef563112598a656e090118b272b6c9cd0f
thomask11 <34552646+thomask11@users.noreply.github.com>
2018-11-05 12:44:21 +0100

    pc to laptop

---
+++ In the Deep Woods/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float jumpForce;
+
+    private Rigidbody2D myRigidBody;
+
+    public bool grounded;
+    public LayerMask Ground;
+
+    private Collider2D myCollider;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        grounded = Physics2D.IsTouchingLayers(myCollider, Ground);
+
+        if(Input.GetKeyDown(KeyCode.Space)  )
+        {
+            if (grounded)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+            }
+        }
+       
+	}
+}
