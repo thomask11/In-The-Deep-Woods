36d04e49852966e86e9ded24940c3814bda9fa7f
Thomas Kraak <thomas8kraak@gmail.com>
2018-11-05 16:41:55 +0100

    nice

--- In the Deep Woods/Assets/Scripts/Player.cs
+++ In the Deep Woods/Assets/Scripts/Player.cs
@@ -11,6 +11,15 @@ public class Player : MonoBehaviour {
     public bool grounded;
     public LayerMask Ground;
 
+    public float timer;
+    public bool isHit;
+    public float hitTime;
+
+    public bool moving;
+    public float speed;
+    public float distance;
+    private float startingpostion;
+
     private Collider2D myCollider;
 
 	// Use this for initialization
@@ -23,6 +32,14 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        if(isHit){
+            timer+=Time.deltaTime;
+        if(timer>=hitTime){
+            isHit = false;
+            timer = 0;
+            }
+        }
+        
         grounded = Physics2D.IsTouchingLayers(myCollider, Ground);
 
         if(Input.GetKeyDown(KeyCode.Space)  )
@@ -32,6 +49,24 @@ public class Player : MonoBehaviour {
                 myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
             }
         }
+
+        if(moving){
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime,transform.position.y);
+            if(startingpostion - transform.position.x >= distance){
+                transform.position = new Vector2(startingpostion - distance,transform.position.y);
+            moving = false;
+            }
+        }
        
 	}
+
+    void OnTriggerEnter2D(Collider2D obstacleHit){
+        if(obstacleHit.gameObject.tag == "ObstacleTrigger" && !isHit){
+            isHit = true;
+            print("hit");
+            obstacleHit.enabled = false;
+            moving = true;
+            startingpostion = transform.position.x;
+        }
+    }
 }
---
+++ In the Deep Woods/Assets/Scripts/ScoreAndGameOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreAndGameOver : MonoBehaviour {
+
+	public bool gameOver;
+	public float score;
+	public Text scoreCount;
+
+	// Use this for initialization
+	void Start () {
+	
+}
+	// Update is called once per frame
+	void Update () {
+		
+		if(!gameOver){
+			score += Time.deltaTime * 5;
+			scoreCount.text = "score: "+ Mathf.RoundToInt(score);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D obstacleHit){
+        if(obstacleHit.gameObject.tag != "ObstacleTrigger" && !gameOver){
+            gameOver = true;
+        }
+    }
+}
